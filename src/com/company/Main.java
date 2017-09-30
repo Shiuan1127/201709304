c4680050a5a50e5b44473b41a3a41c781037a38d
Shiuan1127 <dakotaylor1213@gmail.com>
2017-09-30 15:17:47 +0800

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.*;
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        int a,b,c;
+        a=in.nextInt();
+        b=in.nextInt();
+        c=in.nextInt();
+        System.out.println(a+b+c);
+        System.out.println((a+b+c)/3);
+    }
+}
